83b54516d3a0ec6bac34374313174484df13b1bb
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-07 12:26:59 +0530

    Added Service layer

--- Address/src/main/java/com/AddressBook/Address/controller/AddressController.java
+++ Address/src/main/java/com/AddressBook/Address/controller/AddressController.java
@@ -1,7 +1,10 @@
 package com.AddressBook.Address.controller;
 
+import com.AddressBook.Address.dto.AddressDTO;
 import com.AddressBook.Address.model.Address;
 import com.AddressBook.Address.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,27 +12,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/addresses")
 public class AddressController {
-    private final AddressService service;
+    @Autowired
+    AddressService service;
 
     public AddressController(AddressService service) {
         this.service = service;
     }
 
     @GetMapping
-    public List<Address> getAll() { return service.getAll(); }
-
-    @GetMapping("/{id}")
-    public Address getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Address add(@RequestBody Address address) { return service.save(address); }
-
-    @PutMapping("/{id}")
-    public Address update(@PathVariable Long id, @RequestBody Address address) {
-        address.setId(id);
-        return service.save(address);
+    public ResponseEntity<List<AddressDTO>> getAll() {
+        return ResponseEntity.ok(service.getAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressDTO> getById(@PathVariable Long id) {
+        AddressDTO address = service.getById(id);
+        return address != null ? ResponseEntity.ok(address) : ResponseEntity.notFound().build();
+    }
+    @PostMapping
+    public ResponseEntity<AddressDTO> add(@RequestBody AddressDTO addressDTO) {
+        return ResponseEntity.ok(service.save(addressDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressDTO> update(@PathVariable Long id, @RequestBody AddressDTO addressDTO) {
+        addressDTO.setId(id);
+        return ResponseEntity.ok(service.save(addressDTO));
+    }
+
+
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- Address/src/main/java/com/AddressBook/Address/service/AddressService.java
+++ Address/src/main/java/com/AddressBook/Address/service/AddressService.java
@@ -1,24 +1,27 @@
 package com.AddressBook.Address.service;
 
+import com.AddressBook.Address.dto.AddressDTO;
 import com.AddressBook.Address.model.Address;
 import com.AddressBook.Address.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressService {
+
     private final AddressRepository repository;
 
     public AddressService(AddressRepository repository) {
         this.repository = repository;
     }
 
-    public List<Address> getAll() { return repository.findAll(); }
+    public List<AddressDTO> getAll() {  return repository.findAll().stream()
+            .map(this::convertToDTO)
+            .collect(Collectors.toList()); }
 
-    public Address getById(Long id) { return repository.findById(id).orElse(null); }
+    public AddressDTO getById(Long id) { return repository.findById(id).map(this::convertToDTO).orElse(null); }
 
-    public Address save(Address address) { return repository.save(address); }
-
-    public void delete(Long id) { repository.deleteById(id); }
 }
